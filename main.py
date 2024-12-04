2e89ae74795c3c7c0e7c3fa48ff55acd1fa30184
dylanbrant <dylanpbrant2006@gmail.com>
2024-12-04 19:18:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from cgitb import reset
+
+shapes = ["Square", "Trapezium", "Parallelogram", "Circle", "Triangle", "Rectangle", "Ellipse", "Trapezoid"]
+
+print(shapes)
+
+def choose():
+    global chosen_shape
+    chosen_shape = input("Please write a shape from the list above. \n").lower()
+
+def invalid_shape():
+    print("You input an invalid shape!")
+
+def area_shape():
+    if chosen_shape == ("square"):
+        global square_length
+        square_length = int(input("What is the length of all four sides of the square? \n"))
+        print(f"The area of the square is {square_length*square_length}.")
+    else:(invalid_shape())
+
+choose()
+area_shape()
+
