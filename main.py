1328917cd0e1d39031fe8e2fdf892f48b809bef9
dylanbrant <dylanpbrant2006@gmail.com>
2024-12-08 15:45:08 +0000

    Added support for trapeziums and floats

--- main.py
+++ main.py
@@ -12,8 +12,13 @@ def invalid_shape():
 def area_shape():
     if chosen_shape == ("square"):
         global square_length
-        square_length = int(input("What is the length of all four sides of the square? \n"))
+        square_length = float(input("What is the length of all four sides of the square? \n"))
         print(f"The area of the square is {square_length*square_length}.")
+    elif chosen_shape == ("trapezium"):
+        trapezium_base_length = float(input("What is the base length of the trapezium? \n"))
+        trapezium_perpendicular_height = float(input("What is the perpendicular height of the trapezium? \n"))
+        trapezium_top_length = float(input("What is the top length of the trapezium? \n"))
+        print(f"The area of the trapezium is {(trapezium_base_length+trapezium_top_length)/2*trapezium_perpendicular_height}.")
     else:(invalid_shape())
 
 choose()
